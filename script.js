6a5266743d487fcdbc6c769669f15b8c69a99f97
Justyna Rachowicz <mrowa44@gmail.com>
2018-04-30 16:39:08 +0200

    Add service worker

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ function placeTimer() {
 
 placeTimer();
 setInterval(placeTimer, 1000);
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(function(registration) {
+      console.log(
+        'Service Worker registration successful with scope: ',
+        registration.scope
+      );
+    })
+    .catch(function(err) {
+      console.log('Service Worker registration failed: ', err);
+    });
+}
