fb69c93cb974a9d0e37b38bfd8ddc200c57ddc09
Justyna Rachowicz <mrowa44@gmail.com>
2018-06-12 19:27:11 +0200

    Turn off service worker for now

--- script.js
+++ script.js
@@ -24,16 +24,16 @@ placeTimer();
 setInterval(placeTimer, 1000);
 
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/service-worker.js')
-    .then(function(registration) {
-      console.log(
-        'Service Worker registration successful with scope: ',
-        registration.scope
-      );
-    })
-    .catch(function(err) {
-      console.log('Service Worker registration failed: ', err);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/service-worker.js')
+//     .then(function(registration) {
+//       console.log(
+//         'Service Worker registration successful with scope: ',
+//         registration.scope
+//       );
+//     })
+//     .catch(function(err) {
+//       console.log('Service Worker registration failed: ', err);
+//     });
+// }
--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,35 @@
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(
-    caches.open('static').then(function(cache) {
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/script.js',
-        '/manifest.json',
-        'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.min.js',
-      ]);
-    })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   console.log('[Service Worker] Installing Service Worker ...', event);
+//   event.waitUntil(
+//     caches.open('static').then(function(cache) {
+//       cache.addAll([
+//         '/',
+//         '/index.html',
+//         '/script.js',
+//         '/manifest.json',
+//         'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.min.js',
+//       ]);
+//     })
+//   );
+// });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
-});
+// self.addEventListener('activate', function(event) {
+//   console.log('[Service Worker] Activating Service Worker ....', event);
+// });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request).then(function(res) {
-          return caches.open('dynamic').then(function(cache) {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        });
-      }
-    })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       if (response) {
+//         return response;
+//       } else {
+//         return fetch(event.request).then(function(res) {
+//           return caches.open('dynamic').then(function(cache) {
+//             cache.put(event.request.url, res.clone());
+//             return res;
+//           });
+//         });
+//       }
+//     })
+//   );
+// });
